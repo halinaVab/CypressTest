a4b569eec08c593927dc44662accd886cda42bd5
Galina Vabishchevich <galinavab2802@gmail.com>
2021-11-24 15:32:17 +0300

    update

--- cypress/integration/amazonTests/search.spec.js
+++ cypress/integration/amazonTests/search.spec.js
@@ -7,7 +7,7 @@ describe('Amazon tests', () => {
 	});
 
 	it('C2 Test amazon Search', () => {
-		// comment
+		// comment1
 		cy.percySnapshot("Home")
 		BasePage.search('Iphone');
 		SearchResultPage.isOnSerachResultPage();
