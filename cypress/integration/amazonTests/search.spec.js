6f942ae643f4cf2d19fbe80b9d02634a928c6b1c
Halina <galinavab2802@gmail.com>
2021-11-26 17:49:26 +0300

    Update test > > skip-checks: true

--- cypress/integration/amazonTests/search.spec.js
+++ cypress/integration/amazonTests/search.spec.js
@@ -7,7 +7,7 @@ describe('Amazon tests', () => {
 	});
 
 	it('C2 Test amazon Search', () => {
-		// comment17
+		// comment17аа
 		cy.percySnapshot("Home")
 		BasePage.search('Iphone');
 		SearchResultPage.isOnSerachResultPage();
